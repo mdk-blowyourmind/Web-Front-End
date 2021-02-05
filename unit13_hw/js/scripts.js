2e392bb2915f308233ea873287f4a992711c4be4
mdk-blowyourmind <mdk.blowyourmind@gmail.com>
2021-02-06 05:57:34 +0700

    description

--- unit13_hw/js/scripts.js
+++ unit13_hw/js/scripts.js
@@ -1,8 +1,4 @@
-/*!
-    * Start Bootstrap - Creative v6.0.4 (https://startbootstrap.com/theme/creative)
-    * Copyright 2013-2020 Start Bootstrap
-    * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-creative/blob/master/LICENSE)
-    */
+
     (function($) {
   "use strict"; // Start of use strict
 
@@ -60,4 +56,4 @@
     }
   });
 
-})(jQuery); // End of use strict
+})(jQuery); 
